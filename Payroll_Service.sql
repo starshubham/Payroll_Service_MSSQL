280b6a19733d07a6111e78fa53a8cecb586cba6d
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-22 23:26:49 +0530

    [add] Shubham added UC1 program

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -1,2 +1,8 @@
 --UC1 : Ability to create payroll service database
 create database payroll_services;
+use payroll_services;    --go to the database created by using use payroll_service query
+
+exec sp_databases;   --Show all existing databases in short
+
+select * 
+from sys.databases;   --Show all the existing databases in detailed
