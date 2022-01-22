cbb57aba9f54638ef4047a34d25c0174712a2ee4
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-22 23:50:16 +0530

    [add] Shubham added UC2 Program

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -6,3 +6,16 @@ exec sp_databases;   --Show all existing databases in short
 
 select * 
 from sys.databases;   --Show all the existing databases in detailed
+
+
+---UC2 : Ability create a employee payroll table in the payroll service database
+CREATE TABLE employee_payroll 
+(
+   id int identity primary key,
+   name varchar(Max) Not null,
+   salary money default 1000,
+   startDate DateTime default GetDate()
+);
+
+-- Drop Table employee_payroll; Drop is used to Delete DB or Table
+-- Note:- SQL Queries is case insensitive
