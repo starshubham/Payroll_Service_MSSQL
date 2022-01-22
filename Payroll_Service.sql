9d10a2d2717f84082a8057f759b2fdb4716991be
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-23 00:47:29 +0530

    [add] Shubham modified UC4 program

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -17,12 +17,20 @@ CREATE TABLE employee_payroll
    startDate DateTime default GetDate()
 );
 
--- Drop Table employee_payroll; Drop is used to Delete DB or Table
+--Drop Table employee_payroll; --Drop is used to Delete DB or Table
 -- Note:- SQL Queries is case insensitive
 
 
 --UC3 : Ability to create employee payroll data service database
+INSERT INTO employee_payroll (name,salary,startDate) VALUES
+('Bill', 100000.00, '2018-01-03'),
+('Terisa', 200000.00, '2019-11-13'),
+('Charlie', 300000.00, '2020-05-21');
 Insert into employee_payroll (name,salary,startDate) values('Arjun',5545,GETDATE()),('Madan',54545,GETDATE())
+
+--Delete
+--from employee_payroll
+--Where id=3               # It is used to delete a particular row by using id or name etc..
 
 
 --UC4 : Ability to retrieve all the employee payroll data 
